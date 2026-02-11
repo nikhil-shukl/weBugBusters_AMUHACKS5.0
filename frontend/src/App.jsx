9dd3b95d73321297422dbafb84081ff99dc672c1
Jidnyes <jidnyeshbadgujar06@gmail.com>
2026-02-11 10:12:08 +0530

    Created files to enhance UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectAnalyzer from './pages/ProjectAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectAnalyzer />
+    </div>
+  );
+}
+
+export default App;
