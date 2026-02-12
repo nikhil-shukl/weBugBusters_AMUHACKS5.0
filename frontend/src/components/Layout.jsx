e680ec233b319d7afc70ecb16f9631c4c920a9a6
Nikhil Shukla <nikhilshukla8652@gmail.com>
2026-02-12 08:22:08 +0530

    solve deployment error

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './SidebarTemp';
+import Sidebar from "./SidebarTemp";
 
 const Layout = () => {
   return (
